cad2d88cd3af7a796eebc8a3459fe59b8646c69e
Feng <ery.lee@gmail.com>
2016-10-27 16:30:28 +0800

    first commit

---
+++ Makefile
@@ -0,0 +1,17 @@
+PROJECT = emq_mod_retainer
+PROJECT_DESCRIPTION = Retainer Module
+PROJECT_VERSION = 2.0
+
+BUILD_DEPS = emqttd
+dep_emqttd = git https://github.com/emqtt/emqttd master
+
+TEST_DEPS = cuttlefish
+dep_cuttlefish = git https://github.com/emqtt/cuttlefish
+
+ERLC_OPTS += +'{parse_transform, lager_transform}'
+
+include erlang.mk
+
+app.config::
+	cuttlefish -l info -e etc/ -c etc/emq_mod_retainer.conf -i priv/emq_mod_retainer.schema -d data
+
---
+++ src/emq_mod_retainer_app.erl
@@ -0,0 +1,34 @@
+%%--------------------------------------------------------------------
+%% Copyright (c) 2012-2016 Feng Lee <feng@emqtt.io>.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%--------------------------------------------------------------------
+
+-module(emq_mod_retainer_app).
+
+-behaviour(application).
+
+-export([start/2, prep_stop/1, stop/1]).
+
+start(_Type, _Args) ->
+    Env = application:get_all_env(emq_mod_retainer),
+    {ok, Sup} = emq_mod_retainer_sup:start_link(Env),
+    emq_mod_retainer:load(Env),
+    {ok, Sup}.
+
+prep_stop(State) ->
+    emq_mod_retainer:unload(), State.
+
+stop(_State) ->
+    ok.
+
